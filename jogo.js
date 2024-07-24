d74e2114dcbb5df6f74fce8e7a8e47d878dea260
DiegowSantos93 <diegowillian41@gmail.com>
2024-07-24 19:57:43 -0300

    feat: adição atualizar

--- jogo.js
+++ jogo.js
@@ -2,34 +2,43 @@ const prompt = require ("prompt-sync")();
 
 const jogos = [];
 
-const criar = () => {
+const modelo = () => {
     const nome = prompt('Nome do jogo: ');
     const anoLancamento = prompt('Ano de lançamento: ');
     const duracao = prompt('Duração média em horas: ');
     const preco = prompt('Preço: ');
     const estudio = prompt('Estúdio do jogo: ');
-    const sequencia = prompt('Qual a sequência do jogo: ');
-
+    let sequencia = -1
+    if (listar()){
+        sequencia = prompt('Qual a sequência do jogo? Digite 0 se não houver sequência. ') - 1;
+    };
+    
     if (
         nome != "" &&
         anoLancamento >= 1962 && anoLancamento <= 2024 &&
         duracao > 0 &&
         preco == 0 &&
         estudio != "" &&
-        ((sequencia > 0 && sequencia < jogos.length) || jogos.length == 0)
-    ){
-        jogos.push({
-            nome, anoLancamento, duracao, preco, estudio, sequencia
-        })
-        console.log('Jogo cadastrado com sucesso!')
+        ((sequencia >= -1 && sequencia < jogos.length) || jogos.length == 0)
+    ) {
+        return {nome, anoLancamento, duracao, preco, estudio, sequencia}
     } else {
-        console.log('Dados inválidos.')
+        console.log('Dados inválidos.');
     }
-}
+};
+
+const criar = () => {
+    const jogo = modelo()
+    if (jogo != undefined){
+        jogos.push(jogo)
+        console.log('Jogo cadastrado com sucesso!');
+    }
+};
 
 const listar = () => {
     if (jogos.length == 0){
-        console.log("Nenhum jogo cadastrado.")
+        console.log("Nenhum jogo cadastrado.");
+        return false
     } else {
         jogos.forEach((jogo, indice) => {
             console.log(`
@@ -42,6 +51,23 @@ const listar = () => {
             Sequência: ${jogo.sequencia}
             `)
         })
+        return true
     }
-}
+};
 
+const atualizar = () => {
+    listar()
+    if (!listar()){
+        return
+    }
+
+    const indice = prompt('Qual jogo deseja atualizar? ') - 1;
+    const jogo = modelo()
+
+    if (jogo != undefined && indice >= 0 && indice < jogos.length){
+        jogos[indice] = jogo
+        console.log('Jogo atualizado com sucesso!');
+    } else {
+        console.log('Jogo não atualizado.')
+    }
+};
